c486ed38d7948172553d5b2ea2ed2863f3758efe
Yusuf Nur Faisal <nurfaisalyusuf@gmail.com>
2020-08-30 14:31:52 +0700

    @youngmartin45 - [ update ]

---
+++ phpApp/php73ext/tester/index.php
@@ -0,0 +1,24 @@
+<?php 
+// phpinfo();
+$servername = "db_percona-server";
+$username = "root";
+$password = "secret";
+
+// try {
+//   $conn = new PDO("mysql:host=$servername;port=3306;dbname=test", $username, $password);
+//   // set the PDO error mode to exception
+//   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//   echo "Connected successfully PDO";
+// } catch(PDOException $e) {
+//   echo "Connection failed: " . $e->getMessage();
+// }
+
+$mysqli = new mysqli($servername,$username,$password,"test");
+if ($mysqli -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+  exit();
+} else {
+  echo "Connected successfully mysqli";
+}
+
+?>
